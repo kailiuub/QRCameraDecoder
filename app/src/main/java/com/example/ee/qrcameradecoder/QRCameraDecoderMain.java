78a678bb9d295decc068ae93406c2f9d8f2214d6
kailiuub <kailiu@buffalo.edu>
2017-04-23 18:14:54 -0400

    First Commit

---
+++ app/src/main/java/com/example/ee/qrcameradecoder/QRCameraDecoderMain.java
@@ -0,0 +1,13 @@
+package com.example.ee.qrcameradecoder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QRCameraDecoderMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrcamera_decoder_main);
+    }
+}
